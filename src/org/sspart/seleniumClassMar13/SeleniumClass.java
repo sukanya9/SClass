5690804b8951bfc998f3c6ced444640cee573313
sukanya <sukanya.pasupula9@gmail.com>
2019-03-13 22:54:54 +0530

    "Selenium Example"

--- src/org/sspart/seleniumClassMar13/SeleniumClass.java
+++ src/org/sspart/seleniumClassMar13/SeleniumClass.java
@@ -1,9 +1,19 @@
 package org.sspart.seleniumClassMar13;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
 
 public class SeleniumClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("sukanya");
+		//Thread.sleep(2000);
+		driver.quit();
 
 	}
 
