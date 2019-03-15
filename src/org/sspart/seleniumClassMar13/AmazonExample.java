475dd8b4b9ac1847108eab151a3d931fae772b61
sukanya <sukanya.pasupula9@gmail.com>
2019-03-15 08:14:29 +0530

    "Amazon website Example"

--- src/org/sspart/seleniumClassMar13/AmazonExample.java
+++ src/org/sspart/seleniumClassMar13/AmazonExample.java
@@ -12,13 +12,24 @@ public class AmazonExample {
 		WebDriver driver= new ChromeDriver(); 
 		driver.get("https://www.amazon.in");
 		System.out.println("Opened Amazon website");
+		driver.findElement(By.id("nav-your-amazon")).click();
+		System.out.println("your amazon link");
+		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("sukanyaroyals9@gmail.com");
+		driver.findElement(By.id("continue")).click();
+		driver.findElement(By.name("password")).sendKeys("Amazon@9");
+		driver.findElement(By.id("signInSubmit")).click();
+		System.out.println("login amazon account");
+		
+		//By using id
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
 		System.out.println("Searching performed");
+		//By using xpath
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		Select dropdown = new Select(driver.findElement(By.id("searchDropdownBox")));
 		dropdown.selectByVisibleText("Electronics");
 		System.out.println("selected");
-		//dropdown.click();
+		//driver.findElement(By.tagName("//a[@tabindex='21']"));
+		//System.out.println("Todays deal");
 		driver.quit();	
 
 	}
